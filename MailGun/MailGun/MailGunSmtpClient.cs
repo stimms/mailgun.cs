af29adae50af7a20eceb36fa1105d302e121d17a
stimms <stimms@gmail.com>
2011-07-14 20:34:52 -0600

    Initial add of project file and the such

---
+++ MailGun/MailGun/MailGunSmtpClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+
+namespace MailGun
+{
+    public class MailGunSmtpClient
+    {
+        public void Send(MailMessage message)
+        {
+
+        }
+        
+        public void Send(string sender, string recipients, string subject, string body)
+        {
+
+        }
+
+        public void SendAsync(MailMessage message, object userToken)
+        {
+
+        }
+    }
+}
